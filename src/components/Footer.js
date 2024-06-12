438353b28da04326234a4e670158827f2f8c24e5
kenji <kenji.hana1@outlook.com>
2024-06-12 10:50:15 -0400

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+ <div className='flex felx-col justify-between px-2  space-x-10 md:text-base text-sm  mt-2 
+                    shadow-sm  rounded-md'>
+ </div>
+    </>
+  )
+}
+
+export default Footer
